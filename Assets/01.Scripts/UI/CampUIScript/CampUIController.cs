3bab035e64e3290639a0f90c49a5147d8bfdabce
홍영빈 <youngbeenh@gmail.com>
2026-05-11 10:42:45 +0900

    [Add] selection button
    선택지 버튼 추가
    컨트롤로 버튼 참조

--- Assets/01.Scripts/UI/CampUIScript/CampUIController.cs
+++ Assets/01.Scripts/UI/CampUIScript/CampUIController.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using System.Collections.Generic;
 using TMPro;
+using UnityEngine.UI;
 
 public class CampUIController : MonoBehaviour
 {
@@ -8,7 +9,23 @@ public class CampUIController : MonoBehaviour
     [SerializeField] private List<CampAllyDisplay> _allyRoots;
 
     [Header("Text display")]
+    [SerializeField] private TMP_Text _allyNameText;
     [SerializeField] private TMP_Text _conversationText;
+
+    [Header("Button References")]
+    [SerializeField] private List<Button> _interactionButtons;
+
+    private List<TMP_Text> _interactionButtonTexts;
+
+    private void Start()
+    {
+        Refresh();
+        _interactionButtons.ForEach(button =>
+        {
+            _interactionButtonTexts.Add(button.GetComponentInChildren<TMP_Text>());
+        });
+    }
+
 
 
     private void Refresh()
